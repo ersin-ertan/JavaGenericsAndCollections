c5b4bcdbe0a6881d2de04fa64c32cb1ad3aa59be
ersin <ersin_the_ertan@hotmail.com>
2015-01-03 16:44:01 -0500

    Practice - Generic method taking list of Comparable ond returning comparable, list is constructe of SomeComparable types imprementing Comparable.
    - Boxing and unboxing from Integer to int and comparision with .equals that should do the conversion as well
    
    - List creation with Arrays.asList(1,2,3);
    
    - Override of compareTo

--- app/src/main/java/com/nullcognition/javagenericsandcollections/MainActivity.java
+++ app/src/main/java/com/nullcognition/javagenericsandcollections/MainActivity.java
@@ -2,8 +2,13 @@ package com.nullcognition.javagenericsandcollections;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.security.InvalidParameterException;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +17,39 @@ public class MainActivity extends Activity {
    protected void onCreate(Bundle savedInstanceState){
 	  super.onCreate(savedInstanceState);
 	  setContentView(R.layout.activity_main);
+	  generics();
+   }
+
+   private void generics(){
+	  List<Integer> ints = Arrays.asList(1, 2, 3);
+
+	  for(int n : ints){
+		 Log.e(getClass().getSimpleName(), Integer.toString(n));
+		 if(((Integer)(ints.indexOf(n) + 1)).equals(null)){
+			ints.remove(n); // should remove the last element, unless there is a null evaluation error
+		 }
+	  }
+
+	  // unless one of the elements give the type for the compiler to infer
+	  // List<Comparable> list = Arrays.asList(new SomeComparable(1), new SomeComparable(2), (Comparable)new SomeComparable(3));
+
+	  // an explicit type must be given to the creation of the list
+	  List<Comparable> list = Arrays.<Comparable>asList(new SomeComparable(1), new SomeComparable(2), new SomeComparable(3));
+
+	  genMethod(list);
+
+   }
+
+   private Comparable genMethod(List<Comparable> inTs){
+
+	  Comparable largestT = inTs.get(0); // can be null, what do you return if it is?
+
+	  for(Comparable _inTs : inTs){
+		 if(_inTs.compareTo(largestT) == 1){
+			largestT = _inTs; // cant compare because your don't know if the element is comparable
+		 }
+	  }
+	  return largestT;
    }
 
 
@@ -35,5 +73,24 @@ public class MainActivity extends Activity {
 	  }
 
 	  return super.onOptionsItemSelected(item);
+   }
+}
+
+class SomeComparable implements Comparable {
+
+   SomeComparable(int inI){ i = inI; }
+
+   int i = 0;
+
+   @Override
+   public int compareTo(Object another){
+	  int anotherI = (((SomeComparable)another).i);
+
+	  if(anotherI > i){ return - 1; }
+	  else if(anotherI < i){ return 1; }
+	  else if(anotherI != i){
+		 throw new InvalidParameterException(); // neither equal or comparable
+	  }
+	  return 0;   // equal
    }
 }
