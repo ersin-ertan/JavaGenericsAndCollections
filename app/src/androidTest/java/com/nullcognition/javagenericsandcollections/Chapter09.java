4150f0ddf78771888d0562868fecaed19098e940
ersin <ersin_the_ertan@hotmail.com>
2015-02-26 22:47:20 -0500

    Chapter09 - Define patterns, Vistor and Interpreter(with composite)

---
+++ app/src/androidTest/java/com/nullcognition/javagenericsandcollections/Chapter09.java
@@ -0,0 +1,29 @@
+package com.nullcognition.javagenericsandcollections;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by ersin on 26/02/15 at 9:48 PM
+ */
+
+// Design Patterns - Visitor, Interpreter, Function, Strategy, and Subject-Observer
+   // a data structure is defined by case analysis and recursion
+
+public class Chapter09 extends TestCase {
+
+   public void testVisitor(){ // provide new operations without modifying the classes that define the data structure
+	  //  way of separating an algorithm from an object structure on which it operates
+
+   }
+
+   public void testInterpreter(){ // specifies how to evaluate sentences in a language
+	  // have a class for each symbol, syntax tree of a sentence in the language is an instance of the composite pattern and is used to evaluate
+	  // (interpret) the sentence for a client - composite pattern composite pattern describes that a group of objects is to be treated in the same
+	  // way as a single instance of an object
+
+   }
+
+
+
+
+}
