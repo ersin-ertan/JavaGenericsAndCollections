6fc85a0a5ca0ec490289a25484de397d691827da
ersin <ersin_the_ertan@hotmail.com>
2015-03-14 03:31:55 -0400

    Chapter 09 - Interface and appAll method defined for Function pattern

--- app/src/androidTest/java/com/nullcognition/javagenericsandcollections/designpatterns/Function.java
+++ app/src/androidTest/java/com/nullcognition/javagenericsandcollections/designpatterns/Function.java
@@ -1,5 +1,8 @@
 package com.nullcognition.javagenericsandcollections.designpatterns;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ersin on 02/03/15 at 10:44 PM
  */
@@ -7,5 +10,18 @@ package com.nullcognition.javagenericsandcollections.designpatterns;
 // converts an arbitrary method into an object, similar to the relation between Comparator and the compareTo method
 // use a type variable in the throws clause of a method declaration
 
+interface FunctionInterface <A, B, C extends Throwable> {
 
-public class Function {}
+   B apply(A a) throws C;
+}
+
+public class Function {
+
+   public static <A, B, C extends Throwable> List<B> applyAll(FunctionInterface<A, B, C> function, List<A> list) throws C{
+	  List<B> result = new ArrayList<>(list.size());
+
+	  for(A a : list){ result.add(function.apply(a)); }
+	  return result;
+   }
+
+}
