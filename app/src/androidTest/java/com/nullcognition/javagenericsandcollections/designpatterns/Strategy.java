399dcbc42dc46917790b66332074e13b5ae10a03
ersin <ersin_the_ertan@hotmail.com>
2015-03-14 20:22:07 -0400

    Chapter 09 - Strategy Pattern - Parallel Class Hierarchy - having a connection like the computeTax method for a TaxStrategy that is prallel to a TaxPayer which is expecting an argument that is of the corresponding type which is captured with generics, creating the connection

--- app/src/androidTest/java/com/nullcognition/javagenericsandcollections/designpatterns/Strategy.java
+++ app/src/androidTest/java/com/nullcognition/javagenericsandcollections/designpatterns/Strategy.java
@@ -18,3 +18,61 @@ package com.nullcognition.javagenericsandcollections.designpatterns;
 	  */
 
 public class Strategy {}
+
+class StrategyParallelClassHierarchy { // given a tax strategy, generics will use the type Taxpayer as defined in the TaxStrategy interface, and error if
+   // the type is not compatible at compile time
+
+   // the two parallel class hierarchies are the taxyaper, person and trust
+   // and the other is of strategies coresponding to: DefaultTaxStrategy and DodgingTaxStrategy, as they apply to TaxPayer, non for Person, and one for Trust
+
+   abstract class TaxPayer {
+
+	  public long income; // value in cents, to avoid floating point error
+
+	  public TaxPayer(long inIncome){income = inIncome;}
+
+	  public long getIncome(){return income;}
+   }
+
+   class Person extends TaxPayer {
+
+	  public Person(long income){super(income);}
+   }
+
+   class Trust extends TaxPayer {
+
+	  private boolean nonProfit;
+
+	  public Trust(long income, boolean nonProfit){
+
+		 super(income);
+		 this.nonProfit = nonProfit;
+	  }
+
+	  public boolean isNonProfit(){ return nonProfit; }
+   }
+
+   interface TaxStrategy <P extends TaxPayer> {
+
+	  public long computeTax(P p);
+   }
+
+   class DefaultTaxStrategy <P extends TaxPayer> implements TaxStrategy<P> {
+
+	  private static final double RATE = 0.04;
+
+	  public long computeTax(P payer){return Math.round(payer.getIncome() * RATE);}
+   }
+
+   class DodgingTaxStrategy <P extends TaxPayer> implements TaxStrategy<P> {
+
+	  public long computeTax(P payer){return 0;}
+   }
+
+   class TrustTaxStrategy extends DefaultTaxStrategy<Trust> {
+
+	  public long computeTax(Trust trust){
+		 return trust.isNonProfit() ? 0 : super.computeTax(trust);
+	  }
+   }
+}
